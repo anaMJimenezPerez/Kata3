929024962225d328fdd17943bd5927090baa9411
anam0 <anam0@LAPTOP-VDUU5KM1>
2022-10-19 09:14:16 +0100

    Primera Version Kata3

---
+++ src/kata3/Kata3.java
@@ -0,0 +1,10 @@
+package kata3;
+
+public class Kata3 {
+
+    public static void main(String[] args) {
+        HistogramDisplay histogramDisplay = new HistogramDisplay("Histogram");
+        histogramDisplay.execute();
+    }
+    
+}
